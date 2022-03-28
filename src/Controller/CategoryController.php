73c4167e0c5833361a6f1a2a2040279001cd678e
Lucas <lucas@ld-web.net>
2022-03-28 12:17:24 +0200

    :sparkles: Categories list

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/categories", name="categories_list")
+     */
+    public function list(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render('category/list.html.twig', [
+            'controller_name' => 'CategoryController',
+            'categories' => $categories
+        ]);
+    }
+}
