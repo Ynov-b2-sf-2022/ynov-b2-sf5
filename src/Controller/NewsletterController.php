376221f4202824ed5c1b9c1ec7f98bea163b5e8d
Lucas <lucas@ld-web.net>
2022-04-05 17:44:30 +0200

    :sparkles: Mailer + Service + parameter injection

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Newsletter;
 use App\Form\NewsletterType;
+use App\Mail\NewsletterSubscribed;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,8 +17,11 @@ class NewsletterController extends AbstractController
     /**
      * @Route("/newsletter/register", name="newsletter_register")
      */
-    public function register(Request $request, EntityManagerInterface $em): Response
-    {
+    public function register(
+        Request $request,
+        EntityManagerInterface $em,
+        NewsletterSubscribed $notificationService
+    ): Response {
         $newsletter = new Newsletter();
         $form = $this->createForm(NewsletterType::class, $newsletter);
 
@@ -27,6 +31,7 @@ class NewsletterController extends AbstractController
             $em->persist($newsletter);
             $em->flush();
             $this->addFlash('success', 'Votre email a été enregistré, merci');
+            $notificationService->sendConfirmation($newsletter);
             return $this->redirectToRoute('app_index');
         }
 
